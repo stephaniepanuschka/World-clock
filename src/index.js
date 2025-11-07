cbf925c7a4e71b2011b1aee7b8a4926d57bc6089
Stephanie Panuschka <stephanie.panuschka@gmail.com>
2025-11-07 15:16:14 +0100

    javascript added

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// Johannesburg and London Time Updater
+function updateTime() {
+  // Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  if (johannesburgElement) {
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+
+  // London
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+  }
+}
+
+updateTime();
+setInterval(updateTime, 1000);
