54460e4154cec4211d2209852a9feacda23205cd
Stephanie Panuschka <stephanie.panuschka@gmail.com>
2025-11-07 19:40:36 +0100

    added mutliple location js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,63 @@
-// Johannesburg and London Time Updater
 function updateTime() {
-  // Johannesburg
-  let johannesburgElement = document.querySelector("#johannesburg");
-  if (johannesburgElement) {
-    let johannesburgDateElement = johannesburgElement.querySelector(".date");
-    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
-    let johannesburgTime = moment().tz("Africa/Johannesburg");
+  
+  document.querySelectorAll(".city").forEach((cityElement) => {
+    let cityId = cityElement.getAttribute("id");
+    let timeZone = cityElement.dataset.tz || getTimeZone(cityId);
 
-    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
-    johannesburgTimeElement.innerHTML = johannesburgTime.format("h:mm:ss [<small>]A[</small>]");
-  }
+    if (!timeZone) return;
 
-  // London
-  let londonElement = document.querySelector("#london");
-  if (londonElement) {
-    let londonDateElement = londonElement.querySelector(".date");
-    let londonTimeElement = londonElement.querySelector(".time");
-    let londonTime = moment().tz("Europe/London");
-
-    londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
-  }
+    let cityTime = moment().tz(timeZone);
+    cityElement.querySelector(".date").innerHTML = cityTime.format("MMMM Do YYYY");
+    cityElement.querySelector(".time").innerHTML = cityTime.format("h:mm:ss [<small>]A[</small>]");
+  });
 }
+
+
+function getTimeZone(cityId) {
+  const zones = {
+    johannesburg: "Africa/Johannesburg",
+    london: "Europe/London",
+    berlin: "Europe/Berlin",
+    "sao-paulo": "America/Sao_Paulo",
+    auckland: "Pacific/Auckland",
+  };
+  return zones[cityId];
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (!cityTimeZone) return;
+
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let citiesContainer = document.querySelector("#cities");
+
+
+  if (document.querySelector(`[data-tz="${cityTimeZone}"]`)) {
+    alert(`${cityName} is already displayed.`);
+    return;
+  }
+
+ 
+  let cityElement = document.createElement("div");
+  cityElement.classList.add("city");
+  cityElement.dataset.tz = cityTimeZone;
+  cityElement.innerHTML = `
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date"></div>
+    </div>
+    <div class="time"></div>
+  `;
+  citiesContainer.appendChild(cityElement);
+
+ 
+  updateTime();
+}
+
 
 updateTime();
 setInterval(updateTime, 1000);
+
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
